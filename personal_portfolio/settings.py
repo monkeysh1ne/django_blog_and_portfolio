f1869f94ee674f7482af0fef1f1776fcb1e17f15
monkeysh1ne <mcdoob@gmail.com>
2019-11-27 09:28:24 +1300

    ported sqlite3 db to postgresql

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '23mda5xr7@mq-ns)07+um^ze6^y+1mw2p=((iif$gu769qpwf('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog_db',
+        'USER': 'mypsguser',
+        'PASSWORD': 'adsL0331',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
